d3f784ffa0b411d25e3446bc9b154909f69f1cf6
Conie O'Malley <co1984a@american.edu>
2024-06-17 18:57:49 -0400

    6/17 Class Update

--- Data Visualization/Data Visualization Basics.py
+++ Data Visualization/Data Visualization Basics.py
@@ -1,1 +1,9 @@
-import matplotlib
+
+import matplotlib.pyplot as plt
+
+num_students = [38, 25, 32, 7]
+majors = ['CS', 'Mathematics', 'Engineering', 'Literature']
+
+line1 = plt.plot(majors, num_students)
+
+plt.show()
